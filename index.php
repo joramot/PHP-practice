ebde74d4d5419650bb4d99685c1cdb7c2166e8c1
Jose RM <mojica.raul@gmail.com>
2025-08-02 13:22:08 -0600

    creacion de comentatrios, definicion de variables y operadores aritmeticos

---
+++ index.php
@@ -0,0 +1,16 @@
+<h1>Codigo HTML</h1>
+
+<?php
+// This is a simple PHP script that outputs "Hola Mundo" in HTML format
+    echo "<p>Hola Mundo PHP, la manera mas comun de imprimir a pantalla en
+     PHP es usando la palabra reservada 'echo'</p>";
+    print "<p>Se puede imprimir PHP usando la palabra reservada 'print'</p>";
+
+
+    # usos de variables y opreadores
+    $var_1 = 10;
+    $var_2 = 20;
+
+    echo "<h1>La suma de $var_1 + $var_2 es igual a " . ($var_1 + $var_2) . "</h1>";
+    echo "<h1>La resta de $var_1 - $var_2 es igual a " . ($var_1 - $var_2) . "</h1>";
+?>
